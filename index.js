34452a9ab4672ed35d4e480e38a170df3f46ff4c
pirlo47 <ntoahaep108@gmail.com>
2025-11-20 10:25:21 +0200

    Add /health endpoint

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
+const os = require('os');
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, DevOps!');
+});
+
+//The new system health route
+app.get('/health', (req, res) => {
+    const healthCheck = {
+        uptime: process.uptime(),
+        message: 'OK',
+        timestamp: Date.now(),
+        systemInfo: {
+            platform: os.platform(),
+            release: os.release(),
+            hostname: os.hostname(),
+            totalMemory: `${(os.totalmem()/ 1024 / 1024).toFixed(2)} MB`,
+            freeMemory: `${(os.freemem()/ 1024 / 1024).toFixed(2)} MB`,
+        }
+    };
+    res.send(healthCheck);
 });
 
 app.listen(PORT, ()=> {
